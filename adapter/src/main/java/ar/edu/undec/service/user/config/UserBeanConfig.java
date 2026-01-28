4691cf86fbe5ccd5e58f691f3d9a7cb6668f3c89
Daniel Paez <danypaez142@gmail.com>
2026-01-27 21:01:39 -0300

    feat: controller y config para el caso de uso activar usuario

--- adapter/src/main/java/ar/edu/undec/service/user/config/UserBeanConfig.java
+++ adapter/src/main/java/ar/edu/undec/service/user/config/UserBeanConfig.java
@@ -2,8 +2,11 @@ package ar.edu.undec.service.user.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import user.input.ActivateUserInput;
 import user.input.RegisterUserInput;
+import user.repository.ActivateUserRepository;
 import user.repository.RegisterUserRepository;
+import user.usecase.ActivateUserUseCase;
 import user.usecase.RegisterUserUseCase;
 
 @Configuration
@@ -11,5 +14,10 @@ public class UserBeanConfig {
     @Bean
     RegisterUserInput registerUserInput(RegisterUserRepository repository){
         return new RegisterUserUseCase(repository);
+    }
+
+    @Bean
+    ActivateUserInput activateUserInput(ActivateUserRepository repository){
+        return new ActivateUserUseCase(repository);
     }
 }
