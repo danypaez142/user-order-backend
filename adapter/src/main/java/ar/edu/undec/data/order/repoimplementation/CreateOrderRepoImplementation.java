6987325be6ec0989cf669f09e75c924bb1b07f2f
daniel.paez <danypaez142@gmail.com>
2026-02-03 18:02:25 -0300

    fix: agrego anotaciones faltantes

--- adapter/src/main/java/ar/edu/undec/data/order/repoimplementation/CreateOrderRepoImplementation.java
+++ adapter/src/main/java/ar/edu/undec/data/order/repoimplementation/CreateOrderRepoImplementation.java
@@ -5,10 +5,14 @@ import ar.edu.undec.data.order.mapper.OrderMapper;
 import ar.edu.undec.data.order.repository.OrderRepository;
 import order.model.Order;
 import order.repository.CreateOrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CreateOrderRepoImplementation implements CreateOrderRepository {
     private final OrderRepository repository;
 
+    @Autowired
     public CreateOrderRepoImplementation(OrderRepository repository) {
         this.repository = repository;
     }
--- adapter/src/main/java/ar/edu/undec/data/user/repoimplementation/FindUserRepoImplementation.java
+++ adapter/src/main/java/ar/edu/undec/data/user/repoimplementation/FindUserRepoImplementation.java
@@ -3,14 +3,18 @@ package ar.edu.undec.data.user.repoimplementation;
 import ar.edu.undec.data.user.entity.UserEntity;
 import ar.edu.undec.data.user.mapper.UserMapper;
 import ar.edu.undec.data.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import user.model.User;
 import user.repository.FindUserRepository;
 
 import java.util.Optional;
 
+@Service
 public class FindUserRepoImplementation implements FindUserRepository {
     private final UserRepository repository;
 
+    @Autowired
     public FindUserRepoImplementation(UserRepository repository) {
         this.repository = repository;
     }
