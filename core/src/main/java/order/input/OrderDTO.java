7e5949afce333d05efc0846c1090a4d087595762
daniel.paez <danypaez142@gmail.com>
2026-02-03 17:15:25 -0300

    feat: clases para el proyecto core del caso de uso crear orden

---
+++ core/src/main/java/order/input/OrderDTO.java
@@ -0,0 +1,81 @@
+package order.input;
+
+import order.model.OrderStatus;
+import user.input.UserDTO;
+import user.model.User;
+
+import java.time.LocalDateTime;
+
+public class OrderDTO {
+    private Long id;
+    private UserDTO user;
+    private OrderStatus status;
+    private Double amount;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public OrderDTO() {}
+
+    public OrderDTO(Double amount) {
+        this.amount = amount;
+    }
+
+    public OrderDTO(Long id, UserDTO user, OrderStatus status, Double amount, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.user = user;
+        this.status = status;
+        this.amount = amount;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UserDTO getUser() {
+        return user;
+    }
+
+    public void setUser(UserDTO user) {
+        this.user = user;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
